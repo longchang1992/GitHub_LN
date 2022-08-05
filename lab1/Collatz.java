497625ef93e8f0f98abb6c5be06b900c733590bd
Longchang Ni <110366490+longchang1992@users.noreply.github.com>
2022-08-04 22:55:25 -0700

    Collatz Done

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -14,7 +14,7 @@ public class Collatz {
     }
 
     public static void main(String[] args) {
-        int n = 100;
+        int n = 5;
         System.out.print(n + " ");
         while (n != 1) {
             n = nextNumber(n);
